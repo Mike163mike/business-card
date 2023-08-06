9246276cef6a637a903168d1662e84279314df18
Mike <mick_mick_mick@icloud.com>
2023-08-06 13:10:39 +0400

    regular

---
+++ src/main/java/org/technosoft/businesscard/configuration/SecurityConfig.java
@@ -0,0 +1,13 @@
+package org.technosoft.businesscard.configuration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+//@EnableMethodSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+
+}
